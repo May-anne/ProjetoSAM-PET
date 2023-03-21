89cc6a5f3e2e1efdbcc905f7b6dc3a700d90c80f
Mayanne Gomes <mayane.viviane0511@gmail.com>
2023-03-20 21:30:53 -0300

    add codigo esp32 slave

---
+++ ESP32-Slave/ESP32-Slave.ino
@@ -0,0 +1,45 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+// Define a data structure
+typedef struct struct_message {
+  int a;
+  int b;
+} struct_message;
+
+// Create a structured object
+struct_message myData;
+
+
+// Callback function executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Data received: ");
+  Serial.println(len);
+  Serial.print("Velocidade Value: ");
+  Serial.println(myData.a);
+  Serial.print("Angulo Value: ");
+  Serial.println(myData.b);
+  Serial.println();
+}
+
+void setup() {
+  // Set up Serial Monitor
+  Serial.begin(115200);
+  
+  // Set ESP32 as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Initilize ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Register callback function
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+
+}
